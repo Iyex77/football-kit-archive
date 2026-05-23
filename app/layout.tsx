d4804873e5bdee8023f1693ba7fe9530f8bee3b0
Sergio Gil <sergiogil.93@gmail.com>
2026-05-23 14:10:31 +0200

    feat: change title name & description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Coleccin Camisetas",
+  title: "Colección Camisetas",
   description: "Mi colección personal de camisetas",
 };
 
